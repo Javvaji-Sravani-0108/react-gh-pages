6835819a0f8d64ccff780520c8aaff298f17ad1d
Javvaji-Sravani-0108 <66260883+Javvaji-Sravani-0108@users.noreply.github.com>
2020-06-23 20:53:55 +0530

    Updates

---
+++ precache-manifest.47b86ac74cacf40b2dbd74685fcc1fa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3775b2c75dc54526670da11516aeecad",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "b2e9e05c2d17ecd77e7a",
+    "url": "/react-gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0b4506341999d4d34d4e",
+    "url": "/react-gh-pages/static/js/2.54ac2f8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.54ac2f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e9e05c2d17ecd77e7a",
+    "url": "/react-gh-pages/static/js/main.97ea2ed5.chunk.js"
+  },
+  {
+    "revision": "66e191b5fd3a61b4dd4a",
+    "url": "/react-gh-pages/static/js/runtime-main.732442c1.js"
+  },
+  {
+    "revision": "0e32794fe6805b15af377d3319a32ff8",
+    "url": "/react-gh-pages/static/media/2.0e32794f.jpg"
+  }
+]);
